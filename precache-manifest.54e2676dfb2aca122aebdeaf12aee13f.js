ee6c43aeb5b9929a289c27c9934554dc61684e58
xPh03niX <nothidding001@gmail.com>
2020-02-23 18:39:11 -0400

    Updates

---
+++ precache-manifest.54e2676dfb2aca122aebdeaf12aee13f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3497360a66db859169164495f69f8177",
+    "url": "/reactjs-portfolio/index.html"
+  },
+  {
+    "revision": "2f22d9cbbfacc2ec85c4",
+    "url": "/reactjs-portfolio/static/css/2.903a27b1.chunk.css"
+  },
+  {
+    "revision": "bdbd9f6f85d08e4821f1",
+    "url": "/reactjs-portfolio/static/css/main.fa0fc519.chunk.css"
+  },
+  {
+    "revision": "2f22d9cbbfacc2ec85c4",
+    "url": "/reactjs-portfolio/static/js/2.d6c48508.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/reactjs-portfolio/static/js/2.d6c48508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdbd9f6f85d08e4821f1",
+    "url": "/reactjs-portfolio/static/js/main.70928c28.chunk.js"
+  },
+  {
+    "revision": "0c8369ad66f376ede92a",
+    "url": "/reactjs-portfolio/static/js/runtime-main.5d5c997b.js"
+  },
+  {
+    "revision": "43f62b103c33f53ea8becff3f1725ac6",
+    "url": "/reactjs-portfolio/static/media/Vmarine.43f62b10.jpg"
+  },
+  {
+    "revision": "851ac5cd41c1272715245e19eb765cce",
+    "url": "/reactjs-portfolio/static/media/aguaDeLuz.851ac5cd.png"
+  },
+  {
+    "revision": "e2ae6beb553db8c48aef8bf524a438c3",
+    "url": "/reactjs-portfolio/static/media/myImage.e2ae6beb.png"
+  },
+  {
+    "revision": "ac82d97d170d742395675707f640e98d",
+    "url": "/reactjs-portfolio/static/media/todolist.ac82d97d.png"
+  }
+]);
