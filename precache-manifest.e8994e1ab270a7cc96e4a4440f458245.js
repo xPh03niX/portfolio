8b3bdd7b74cf95f90446bb6e4dfacad4cce60a3b
xPh03niX <nothidding001@gmail.com>
2020-03-11 05:29:04 -0400

    Updates

---
+++ precache-manifest.e8994e1ab270a7cc96e4a4440f458245.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6abcacff582d8069ed149539e8698b20",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2158bb0cf7818a5e5c82",
+    "url": "/portfolio/static/css/2.903a27b1.chunk.css"
+  },
+  {
+    "revision": "1927998f629d7d8bcbe4",
+    "url": "/portfolio/static/css/main.e03b5da7.chunk.css"
+  },
+  {
+    "revision": "2158bb0cf7818a5e5c82",
+    "url": "/portfolio/static/js/2.01b0f067.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/portfolio/static/js/2.01b0f067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1927998f629d7d8bcbe4",
+    "url": "/portfolio/static/js/main.f2f7339e.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "d74d7a0ae9c2728abca50f1b17542b0f",
+    "url": "/portfolio/static/media/movies.d74d7a0a.jpg"
+  },
+  {
+    "revision": "6fb0ed2dbca05dc97b1d502190928338",
+    "url": "/portfolio/static/media/valentine.6fb0ed2d.jpg"
+  },
+  {
+    "revision": "079d357220e1bb2ac74b802821b47ebf",
+    "url": "/portfolio/static/media/weather.079d3572.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7dd916b5026deea249a06860343d9e9b.js"
+  "/portfolio/precache-manifest.e8994e1ab270a7cc96e4a4440f458245.js"
 );
 
 self.addEventListener('message', (event) => {
